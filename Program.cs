dc46ab24a9984e95af4dd9c9f94c9ce327f5f91e
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-12-04 16:18:03 -0500

    feat: :sparkles: Test Completed

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Ingrese el multiplicador: ");
+int multiplicador = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ingrese el multiplicando: ");
+int multiplicando = Convert.ToInt32(Console.ReadLine());
+
+int resultado = 0;
+
+while (multiplicador > 0)
+{
+    if (multiplicador %2 !=0)
+    {
+        resultado += multiplicando;
+    }
+    multiplicador = multiplicador / 2;
+    multiplicando = multiplicando * 2;
+}
+
+Console.WriteLine($"Resultado: {resultado}");
